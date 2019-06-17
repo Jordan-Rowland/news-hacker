916d36dd0fc17f181b6bbadbf0eaca338f5d0615
Jordan <jordanrowland00@gmail.com>
2019-06-17 10:14:28 -0700

    realizing I need a comment component

---
+++ src/components/Comment.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+/* jshint esversion: 9 */
+export default {
+  name: 'comment',
+  data() {
+    return {
+
+    };
+  },
+
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/components/Story.vue
+++ src/components/Story.vue
@@ -48,10 +48,53 @@ export default {
   computed: {
   },
   methods: {
-    getChildComments(childElement) {
-      for (let commentId of childElement.kids) {
+    getStory() {
+      // eslint-disable-next-line
+      // console.log('Getting Story ', this.storyId)
+      fetch(`https://hacker-news.firebaseio.com/v0/item/${this.storyId}/.json`)
+        .then(res => res.json())
+        .then(jsonRes => this.story = jsonRes)
+        .then(() => this.getComments(this.story))
+    },
+    getComments(parentElement) {
+      for (let commentId of parentElement.kids) {
         // eslint-disable-next-line
-        console.log('Getting Child Comments', commentId)
+        console.log('Getting parent comment', commentId)
+        fetch(`https://hacker-news.firebaseio.com/v0/item/${commentId}/.json`)
+          .then(res => res.json())
+          .then(jsonRes => {
+            this.comments.push({
+              id: jsonRes.id,
+              parent: jsonRes.parent,
+              by: jsonRes.by,
+              text: jsonRes.text,
+              kids: jsonRes.kids,
+              comments: [],
+          });
+            // console.log('Getting Comment', commentId)
+            // console.log(jsonRes)
+            if (jsonRes.kids.length &&
+                jsonRes.text.length) {
+              for (let childCommentId of jsonRes.kids) {
+                // eslint-disable-next-line
+                console.log('Getting child', childCommentId)
+                this.getChildComments(childCommentId)
+              }
+            }
+          }) // End .then jsonRes
+          // .then((jsonRes) => {
+          //   for (let childCommentId of jsonRes) {
+          //     console.log('Getting child comment', commentId)
+          //     // eslint-disable-next-line
+          //     this.getChildComments(childCommentId)
+          //   }
+          // })
+      }
+    }, // End getComments
+    getChildComments(childElement) {
+      for (let commentId of childElement) {
+        // eslint-disable-next-line
+        // console.log('Getting Child Comments', commentId)
         fetch(`https://hacker-news.firebaseio.com/v0/item/${commentId}/.json`)
           .then(res => res.json())
           .then(jsonRes => childElement.comments.push({
@@ -61,51 +104,19 @@ export default {
             text: jsonRes.text,
             kids: jsonRes.kids,
             comments: [],
-          }))
-      }
-    },
-    getStory() {
-      // eslint-disable-next-line
-      console.log('Getting Story ', this.storyId)
-      fetch(`https://hacker-news.firebaseio.com/v0/item/${this.storyId}/.json`)
-        .then(res => res.json())
-        .then(jsonRes => this.story = jsonRes)
-        .then(() => this.getComments(this.story))
-        .then(() => {
-          // console.log('NEXT THEN')
-          // if (this.comments.length) {
-          //   console.log('Comments exist')
-          // }
-          //   for (let childComment of this.comments) {
-          //     // eslint-disable-next-line
-          //     console.log('Comment of comment', childComment)
-          //     getChildComments(childComment)
-          //   }
-        })
-    },
-    getComments(parentElement) {
-      for (let commentId of parentElement.kids) {
-        // eslint-disable-next-line
-        console.log('Getting Comment', commentId)
-        fetch(`https://hacker-news.firebaseio.com/v0/item/${commentId}/.json`)
-          .then(res => res.json())
-          .then(jsonRes => this.comments.push({
-            id: jsonRes.id,
-            parent: jsonRes.parent,
-            by: jsonRes.by,
-            text: jsonRes.text,
-            kids: jsonRes.kids,
-            comments: [],
-          }))
+          })) // end .then jsonRes
           .then(() => {
-            for (let childComment of this.comments) {
-              // eslint-disable-next-line
-            this.getChildComments(childComment)
+            if (childElement.comments.length) {
+              // console.log('Getting Comment', commentId)
+              // console.log('childElement.comments.length')
+              for (let childOfChild of childElement.comments) {
+                this.getChildComments(childOfChild)
+              }
             }
-          })
-      }
-    },
-  },
+          }) // End then if childElement.comments.length
+      } // End for
+    }, // end getChildComments
+  }, // End methods
   created() {
     this.getStory();
   }
@@ -115,5 +126,7 @@ export default {
 <style scoped>
 .comment {
   border: solid black 2px;
+  margin-left: 5px;
+  padding: 3px;
 }
 </style>
